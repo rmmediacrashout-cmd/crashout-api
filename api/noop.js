2fde631d3c0b305d9b49888002f3fc33c62ea475
rmmediacrashout-cmd <rmmediacrashout@gmail.com>
2026-03-04 18:51:19 +0100

    Update noop.js

--- api/noop.js
+++ api/noop.js
@@ -1,3 +1,10 @@
 export default function handler(req, res) {
-  res.status(200).json({ ok: true });
+  const { play_with, alcohol, location, level } = req.body;
+
+  res.status(200).json({
+    play_with,
+    alcohol,
+    location,
+    level
+  });
 }
